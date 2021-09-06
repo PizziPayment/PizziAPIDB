bedfd69048e1cd9a116bf58de4467adeb818998e
Lilian VERLHAC <lilian.verlhac@outlook.com>
2021-09-06 19:45:08 +0100

    Add shop model abstraction

--- package/src/index.ts
+++ package/src/index.ts
@@ -2,6 +2,7 @@ export * from './credentials/credentials.database.service'
 export * from './tokens/tokens.database.service'
 export * from './clients/clients.database.service'
 export * from './users/users.database.service'
+export * from './shops/shops.database.service'
 
 export * from './commons/services/encryption/encryption.service'
 export * from './init'
---
+++ package/src/shops/shops.database.service.ts
@@ -0,0 +1,52 @@
+import { err, ok, Result } from 'neverthrow'
+import ShopModel from './models/shop.model'
+import Shop from '../commons/services/orm/models/shop.database.model'
+
+export type ShopServiceResult<T> = Result<T, ShopServiceError>
+
+export enum ShopServiceError {
+  DatabaseError,
+  ShopNotFound,
+}
+
+export default class ShopServices {
+  static async deleteShopById(shop_id: number): Promise<ShopServiceResult<null>> {
+    try {
+      const shop = await Shop.findOne({ where: { id: shop_id } })
+
+      if (!shop) {
+        return err(ShopServiceError.ShopNotFound)
+      } else {
+        await shop.destroy()
+        return ok(null)
+      }
+    } catch {
+      return err(ShopServiceError.DatabaseError)
+    }
+  }
+  static async createShop(
+    name: string,
+    phone: string,
+    address: string,
+    zipcode: number
+  ): Promise<ShopServiceResult<ShopModel>> {
+    try {
+      const shop = await Shop.create({
+        address: address,
+        name: name,
+        phone: phone,
+        description: undefined,
+        zipcode: zipcode,
+        logo: undefined,
+        facebook: undefined,
+        instagram: undefined,
+        twitter: undefined,
+        website: undefined,
+      })
+
+      return ok(shop)
+    } catch {
+      return err(ShopServiceError.DatabaseError)
+    }
+  }
+}
