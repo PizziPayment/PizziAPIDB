aa68846f699c5ab0982ab0783c90f3fac0cac337
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-01-18 21:17:49 +0000

    Add transaction db model

---
+++ pizzi-db/src/commons/services/orm/models/transaction.database.model.ts
@@ -0,0 +1,31 @@
+import { AutoIncrement, Column, ForeignKey, Model, PrimaryKey, Table } from 'sequelize-typescript'
+import User from './user.database.model'
+import Receipt from './receipts.database.model'
+
+interface TransactionAttributes {
+  id: number
+  state: number
+  user_id: number
+  receipt_id: number
+}
+
+export type TransactionCreation = Omit<TransactionAttributes, 'id'>
+
+@Table({ tableName: 'transactions', timestamps: false })
+export default class Transaction extends Model<TransactionAttributes, TransactionCreation> {
+  @PrimaryKey
+  @AutoIncrement
+  @Column
+  declare id: number
+
+  @Column
+  state!: number
+
+  @Column
+  @ForeignKey(() => User)
+  user_id?: number
+
+  @Column
+  @ForeignKey(() => Receipt)
+  receipt_id!: number
+}
