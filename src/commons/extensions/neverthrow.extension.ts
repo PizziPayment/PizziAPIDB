bf93377eb8db494c6b7770c1ef49108a5c366b8e
Mathieu Pointecouteau <mathieu.pointecouteau@epitech.eu>
2022-05-09 21:37:23 +0200

    Apply requested changement.

--- src/commons/extensions/neverthrow.extension.ts
+++ src/commons/extensions/neverthrow.extension.ts
@@ -4,7 +4,7 @@ export function okIfNotNullElse<T, E>(error: E): (t: T | null) => ResultAsync<T,
   return (t) => (t ? okAsync(t) : errAsync(error))
 }
 
-export function okIfOneElse<T, E>(error: E): (results: [number, T[]]) => ResultAsync<T, E> {
+export function okIfOneElse<T, E>(error: E): (results: [number, Array<T>]) => ResultAsync<T, E> {
   return (results) => {
     if (results[0] === 1) {
       return okAsync(results[1][0])
