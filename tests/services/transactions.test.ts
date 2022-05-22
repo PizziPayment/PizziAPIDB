04e043a995b3026c453eadc9eed718cb41fefb85
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-05-22 19:14:30 +0100

    Test expanded transaction

--- tests/services/transactions.test.ts
+++ tests/services/transactions.test.ts
@@ -131,7 +131,29 @@ describe('Transaction domain', () => {
     }
   })
 
-  it('should be to retrieve every pending transaction associated to a specific owner', async () => {
+  it("should be able to change a transaction's user_id", async () => {
+    const transaction = await sequelize.transaction()
+    try {
+      const [receipt, user, shop] = await setupReceiptUserAndShop(transaction)
+      const created_transaction = (
+        await TransactionsService.createPendingTransaction(receipt.id, null, shop.id, 'card', transaction)
+      )._unsafeUnwrap()
+
+      expect(
+        (await TransactionsService.updateTransactionUserIdFromId(created_transaction.id, user.id, transaction)).isOk()
+      ).toBeTruthy()
+
+      const retrieved_transaction = (
+        await TransactionsService.getTransactionById(created_transaction.id, transaction)
+      )._unsafeUnwrap()
+
+      expect(retrieved_transaction.user_id).toBe(user.id)
+    } finally {
+      await transaction.rollback()
+    }
+  })
+
+  it('should be able to retrieve every pending transaction associated to a specific owner', async () => {
     const transaction = await sequelize.transaction()
 
     try {
@@ -157,6 +179,116 @@ describe('Transaction domain', () => {
       expect(shop_transaction).toHaveLength(1)
       expect(user_transactions[0].id).toBe(created_transaction.id)
       expect(shop_transaction[0].id).toBe(created_transaction.id)
+    } finally {
+      await transaction.rollback()
+    }
+  })
+
+  it('should be able to retrieve every pending transaction associated to a specific owner', async () => {
+    const transaction = await sequelize.transaction()
+
+    try {
+      const [receipt, user, shop] = await setupReceiptUserAndShop(transaction)
+      const created_transaction = (
+        await TransactionsService.createPendingTransaction(receipt.id, user.id, shop.id, 'card', transaction)
+      )._unsafeUnwrap()
+
+      ;(
+        await TransactionsService.createPendingTransaction(receipt.id, user.id, shop.id, 'card', transaction).map(
+          (trans) => TransactionsService.updateTransactionStateFromId(trans.id, 'validated', transaction)
+        )
+      )._unsafeUnwrap()
+
+      const user_transactions = (
+        await TransactionsService.getOwnerTransactionsByState('user', user.id, 'pending', transaction)
+      )._unsafeUnwrap()
+      const shop_transaction = (
+        await TransactionsService.getOwnerTransactionsByState('shop', shop.id, 'pending', transaction)
+      )._unsafeUnwrap()
+
+      expect(user_transactions).toHaveLength(1)
+      expect(shop_transaction).toHaveLength(1)
+      expect(user_transactions[0].id).toBe(created_transaction.id)
+      expect(shop_transaction[0].id).toBe(created_transaction.id)
+    } finally {
+      await transaction.rollback()
+    }
+  })
+
+  it("should be able to retrieve an users' expanded transaction by state", async () => {
+    const transaction = await sequelize.transaction()
+
+    try {
+      const [receipt, user, shop] = await setupReceiptUserAndShop(transaction)
+      const created_transaction = (
+        await TransactionsService.createPendingTransaction(receipt.id, user.id, shop.id, 'card', transaction)
+      )._unsafeUnwrap()
+
+      const expanded_transactions = (
+        await TransactionsService.getOwnerExpandedTransactionsByState("user", user.id, "pending", transaction)
+      )._unsafeUnwrap()
+
+      expect(expanded_transactions).toHaveLength(1)
+      expect(expanded_transactions[0].id).toBe(created_transaction.id)
+      expect(expanded_transactions[0].user_id).toBe(created_transaction.user_id)
+      expect(expanded_transactions[0].user_id).toBe(user.id)
+      expect(expanded_transactions[0].shop.id).toBe(created_transaction.shop_id)
+      expect(expanded_transactions[0].shop.id).toBe(shop.id)
+      expect(expanded_transactions[0].shop.name).toBe(shop.name)
+      expect(expanded_transactions[0].state).toBe(created_transaction.state)
+      expect(expanded_transactions[0].receipt.id).toBe(receipt.id)
+      expect(expanded_transactions[0].receipt.total_ht).toBe(receipt.total_price)
+      expect(expanded_transactions[0].receipt.tva_percentage).toBe(receipt.tva_percentage)
+      expect(expanded_transactions[0].created_at.toString()).toBe(created_transaction.created_at.toString())
+      expect(expanded_transactions[0].updated_at?.toString()).toBe(created_transaction.updated_at?.toString())
+    } finally {
+      await transaction.rollback()
+    }
+  })
+  it("should be able to retrieve a shop's expanded transaction by state", async () => {
+    const transaction = await sequelize.transaction()
+
+    try {
+      const [receipt, user, shop] = await setupReceiptUserAndShop(transaction)
+      const created_transaction = (
+        await TransactionsService.createPendingTransaction(receipt.id, user.id, shop.id, 'card', transaction)
+      )._unsafeUnwrap()
+
+      const expanded_transactions = (
+        await TransactionsService.getOwnerExpandedTransactionsByState("shop", shop.id, "pending", transaction)
+      )._unsafeUnwrap()
+
+      expect(expanded_transactions).toHaveLength(1)
+      expect(expanded_transactions[0].id).toBe(created_transaction.id)
+      expect(expanded_transactions[0].user_id).toBe(created_transaction.user_id)
+      expect(expanded_transactions[0].user_id).toBe(user.id)
+      expect(expanded_transactions[0].shop.id).toBe(created_transaction.shop_id)
+      expect(expanded_transactions[0].shop.id).toBe(shop.id)
+      expect(expanded_transactions[0].shop.name).toBe(shop.name)
+      expect(expanded_transactions[0].state).toBe(created_transaction.state)
+      expect(expanded_transactions[0].receipt.id).toBe(receipt.id)
+      expect(expanded_transactions[0].receipt.total_ht).toBe(receipt.total_price)
+      expect(expanded_transactions[0].receipt.tva_percentage).toBe(receipt.tva_percentage)
+      expect(expanded_transactions[0].created_at.toString()).toBe(created_transaction.created_at.toString())
+      expect(expanded_transactions[0].updated_at?.toString()).toBe(created_transaction.updated_at?.toString())
+    } finally {
+      await transaction.rollback()
+    }
+  })
+  it("should be able to retrieve a shop's expanded transaction by state (shouldn't retrieve any)", async () => {
+    const transaction = await sequelize.transaction()
+
+    try {
+      const [receipt, user, shop] = await setupReceiptUserAndShop(transaction)
+      const created_transaction = (
+        await TransactionsService.createPendingTransaction(receipt.id, user.id, shop.id, 'card', transaction)
+      )._unsafeUnwrap()
+
+      const expanded_transactions = (
+        await TransactionsService.getOwnerExpandedTransactionsByState("shop", shop.id, "validated", transaction)
+      )._unsafeUnwrap()
+
+      expect(expanded_transactions).toHaveLength(0)
     } finally {
       await transaction.rollback()
     }
