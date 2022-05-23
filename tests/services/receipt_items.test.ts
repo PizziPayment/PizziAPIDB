c0423ba1bf222b803516419aff91616685692656
Lilian VERLHAC <lilian.verlhac@outlook.com>
2022-05-23 04:34:08 +0100

    Add bulk create for receipt_items

--- tests/services/receipt_items.test.ts
+++ tests/services/receipt_items.test.ts
@@ -76,4 +76,57 @@ describe('Receipt items domain', () => {
       await transaction.rollback()
     }
   })
+  it('should be able to bulk create receipt items', async () => {
+    const receipt_sample: Omit<ReceiptModel, 'id'> = {
+      tva_percentage: 10,
+      total_price: '4',
+    }
+    const shop_items_sample: Array<ShopItemCreationModel> = [
+      {
+        name: 'liver',
+        price: '10',
+      },
+      {
+        name: 'kidney',
+        price: '4',
+      },
+    ]
+    const transaction = await sequelize.transaction()
+
+    try {
+      const created_receipt = (
+        await ReceiptsService.createReceipt(receipt_sample.tva_percentage, receipt_sample.total_price, transaction)
+      )._unsafeUnwrap()
+
+      expect(
+        (
+          await ShopsServices.createShop('test', '0202020202', 1232132131, 'address', 'city', 20000, transaction).map(
+            (shop) =>
+              ShopItemsService.createShopItems(shop.id, shop_items_sample, transaction).map((shop_items) =>
+                ReceiptItemsService.createReceiptItems(
+                  created_receipt.id,
+                  shop_items.map((shop_item) => {
+                    return {
+                      eco_tax: 0,
+                      discount: 0,
+                      quantity: 1,
+                      warranty: 'fdsfsf',
+                      shop_item_id: shop_item.id,
+                    }
+                  }),
+                  transaction
+                )
+              )
+          )
+        ).isOk()
+      )
+      const retrieved_items = (
+        await ReceiptItemsService.getReceiptItems(created_receipt.id, transaction)
+      )._unsafeUnwrap()
+
+      expect(retrieved_items).toHaveLength(2)
+    } finally {
+      await transaction.rollback()
+    }
+  })
 })
