9b7b254ecf5793d7928e68d47d2215b3b2f92695
agarof <alban.garofoli@epitech.eu>
2022-11-27 16:59:35 +0100

    Create AdminModel interface

--- src/admins/admins.database.service.ts
+++ src/admins/admins.database.service.ts
@@ -3,9 +3,15 @@ import { Transaction } from 'sequelize/types'
 import { PizziError, PizziResult } from '../commons/models/service.error.model'
 import Admin from '../commons/services/orm/models/admins.database.model'
 
+export interface AdminModel {
+  id: number
+}
+
 export class AdminsService {
-  static createAdmin(transaction?: Transaction): PizziResult<Admin> {
-    return ResultAsync.fromPromise(Admin.create(undefined, { transaction }), () => PizziError.internalError())
+  static createAdmin(transaction?: Transaction): PizziResult<AdminModel> {
+    return ResultAsync.fromPromise(Admin.create(undefined, { transaction }), () => PizziError.internalError()).map(
+      to_model
+    )
   }
 
   static deleteAdminById(id: number, transaction?: Transaction): PizziResult<void> {
@@ -14,9 +20,13 @@ export class AdminsService {
     )
   }
 
-  static getAdminsPage(page: number, nb_items: number, transaction?: Transaction): PizziResult<Admin[]> {
+  static getAdminsPage(page: number, nb_items: number, transaction?: Transaction): PizziResult<AdminModel[]> {
     return ResultAsync.fromPromise(Admin.findAll({ limit: nb_items, offset: (page - 1) * nb_items, transaction }), () =>
       PizziError.internalError()
-    )
+    ).map((arr) => arr.map(to_model))
   }
+}
+
+function to_model(admin: Admin): AdminModel {
+  return { id: admin.id }
 }
